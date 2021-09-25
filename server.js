d75858dd42eb1580368d2bd425477fe34dae65ba
unknown <pronojit@protonmail.ch>
2021-09-25 20:12:23 +0530

    initial commmit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+//importing routes
+const bootcamps = require("./routes/bootcamps.js");
+
+//dotenv
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+//router/routes
+app.use("/", bootcamps);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () =>
+  console.log(`STATUS:Active in ${process.env.NODE_ENV} on ${port}`)
+);
